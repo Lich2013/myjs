0b35bdfe340163e413a589e22f7c3b8f84e9efa4
Lich2013 <longzy2013@gmail.com>
2014-12-07 02:36:00 +0800

    回调函数,tieba2.0,weibo1.0

---
+++ weibo.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name           delete weibo ad
+// @description    Yes
+// @version        1.0
+// @author         Lich
+// @namespace      http://weibo.com/
+// @include        http://weibo.com/*
+// ==/UserScript==
+function a(a){
+	setTimeout(function () {
+		a(a);
+	}, 0)
+	ad = document.querySelectorAll('div.WB_right_module.M_sc_right');
+	for (var i = 0; i < ad.length; i++) {
+			ad[i].parentNode.removeChild(ad[i]);
+		};	
+}
+a(a);
